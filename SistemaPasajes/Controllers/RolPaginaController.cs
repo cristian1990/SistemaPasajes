06fdf80e21b830fb65a719a7a6d0effaf3145c0e
Crismo90 <cristian___777@hotmail.com>
2020-08-12 15:23:46 -0300

    Interfaz gráfica para registrar usuario u cifrado de clave

---
+++ SistemaPasajes/Controllers/RolPaginaController.cs
@@ -0,0 +1,148 @@
+﻿using SistemaPasajes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaPasajes.Controllers
+{
+    public class RolPaginaController : Controller
+    {
+        // GET: RolPagina
+        public ActionResult Index()
+        {
+            listarComboRol();
+            listarComboPagina();
+            List<RolPaginaCLS> listaRol = new List<RolPaginaCLS>();
+            using (var bd = new BDPasajeEntities())
+            {
+                //Utilizando LINQ
+                listaRol = (from rolpagina in bd.RolPagina
+                            join rol in bd.Rol
+                            on rolpagina.IIDROL equals rol.IIDROL
+                            join pagina in bd.Pagina
+                            on rolpagina.IIDPAGINA equals pagina.IIDPAGINA
+                            where rolpagina.BHABILITADO == 1
+                            select new RolPaginaCLS //Creo un objeto
+                            {   //Le asigno valores al objeto
+                                iidrolpagina = rolpagina.IIDROLPAGINA,
+                                nombreRol = rol.NOMBRE,
+                                nombreMensaje = pagina.MENSAJE
+                            }).ToList();
+            }
+            return View(listaRol);
+        }
+
+        public ActionResult Filtrar(int? iidrol) //Puede aceptar null
+        {
+
+            List<RolPaginaCLS> listaRol = new List<RolPaginaCLS>();
+            using (var bd = new BDPasajeEntities())
+            {
+                //Si no se ingreso nada para filtrar
+                if (iidrol == null)
+                {
+                    //Listamos todo
+                    listaRol = (from rolpagina in bd.RolPagina
+                                join rol in bd.Rol
+                                on rolpagina.IIDROL equals
+                                rol.IIDROL
+                                join pagina in bd.Pagina
+                                on rolpagina.IIDPAGINA equals
+                                pagina.IIDPAGINA
+                                where rolpagina.BHABILITADO == 1
+                                select new RolPaginaCLS
+                                {
+                                    iidrolpagina = rolpagina.IIDROLPAGINA,
+                                    nombreRol = rol.NOMBRE,
+                                    nombreMensaje = pagina.MENSAJE
+                                }).ToList();
+                }
+                else //Si se ingreso algo para filtrar
+                {
+                    listaRol = (from rolpagina in bd.RolPagina
+                                join rol in bd.Rol
+                                on rolpagina.IIDROL equals
+                                rol.IIDROL
+                                join pagina in bd.Pagina
+                                on rolpagina.IIDPAGINA equals
+                                pagina.IIDPAGINA
+                                where rolpagina.BHABILITADO == 1
+                                && rolpagina.IIDROL == iidrol //Filtro por el idRol ingresado
+                                select new RolPaginaCLS
+                                {
+                                    iidrolpagina = rolpagina.IIDROLPAGINA,
+                                    nombreRol = rol.NOMBRE,
+                                    nombreMensaje = pagina.MENSAJE
+                                }).ToList();
+                }
+
+            }
+            //Retorno a la vista parcial y envio la lista
+            return PartialView("_TablaRolPagina", listaRol);
+        }
+
+        public int Guardar(RolPaginaCLS oRolPaginaCLS, int titulo)
+        {
+            int rpta = 0; //Nro de registros afectados
+
+            using (var bd = new BDPasajeEntities())
+            {
+                if (titulo == 1) //Si es 1, se quiere agregar
+                {
+                    RolPagina oRolPagina = new RolPagina();
+                    oRolPagina.IIDROL = oRolPaginaCLS.iidrol;
+                    oRolPagina.IIDPAGINA = oRolPaginaCLS.iidpagina;
+                    oRolPagina.BHABILITADO = 1;
+                    bd.RolPagina.Add(oRolPagina);
+                    //SaveChanges: devuelve el numero de registros afectados
+                    rpta = bd.SaveChanges();
+                }
+            }
+
+            return rpta;
+        }
+
+
+        //============================================================
+
+        //PARA LLENAR TODOS LOS COMBOBOX
+        //Creo todas las listas necesarias y las paso a la vista
+        public void listarComboRol()
+        {
+            //agregar
+            List<SelectListItem> lista;
+            using (var bd = new BDPasajeEntities())
+            {
+                lista = (from item in bd.Rol
+                         where item.BHABILITADO == 1
+                         select new SelectListItem
+                         {
+                             Text = item.NOMBRE,
+                             Value = item.IIDROL.ToString()
+                         }).ToList();
+                lista.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+                ViewBag.listaRol = lista;
+            }
+        }
+
+        public void listarComboPagina()
+        {
+            //agregar
+            List<SelectListItem> lista;
+            using (var bd = new BDPasajeEntities())
+            {
+                lista = (from item in bd.Pagina
+                         where item.BHABILITADO == 1
+                         select new SelectListItem
+                         {
+                             Text = item.MENSAJE,
+                             Value = item.IIDPAGINA.ToString()
+                         }).ToList();
+                lista.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+                ViewBag.listaPagina = lista;
+            }
+        }
+    }
+}
--- SistemaPasajes/Controllers/UsuarioController.cs
+++ SistemaPasajes/Controllers/UsuarioController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using SistemaPasajes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Transactions; //Lo agregue desde las referencias
+using System.Security.Cryptography;
+using System.Text; //Para Byte
 
 namespace SistemaPasajes.Controllers
 {
@@ -11,7 +15,104 @@ namespace SistemaPasajes.Controllers
         // GET: Usuario
         public ActionResult Index()
         {
+            listaPersonas();
+            listarRol();
+
             return View();
+        }
+
+        //Utilizo transacciones, porque voy a tener que manipular en 2 tablas, para insertar y actualizar
+        //la de Usuario y la de cliente o Empleado
+        public int Guardar(UsuarioCLS oUsuarioCLS, int titulo) //titulo viene de la vista
+        {
+            int rpta = 0;
+
+            try 
+            {
+                using (var bd = new BDPasajeEntities())
+                {
+                    //Abro un bloque de transaccion
+                    using (var transaccion = new TransactionScope())
+                    {
+                        if (titulo == 1) //1: significa Agregar
+                        {
+                            Usuario oUsuario = new Usuario();
+                            oUsuario.NOMBREUSUARIO = oUsuarioCLS.nombreusuario;
+                            SHA256Managed sha = new SHA256Managed(); //Para cifrar la clave
+                            byte[] byteContra = Encoding.Default.GetBytes(oUsuarioCLS.contra); //Convierto a array de Byte
+                            byte[] byteContraCifrado = sha.ComputeHash(byteContra); //Cifro
+                            string cadenaContraCifrada = BitConverter.ToString(byteContraCifrado).Replace("-", ""); //Convierto a cadena
+                            oUsuario.CONTRA = cadenaContraCifrada;
+                            //oUsuario.TIPOUSUARIO = oUsuarioCLS.nombrePersonaEnviar.Substring(oUsuarioCLS.nombrePersonaEnviar.Length - 2, 1);
+                            //oUsuario.IID = oUsuarioCLS.iid;
+                            //oUsuario.IIDROL = oUsuarioCLS.iidrol;
+                            //oUsuario.bhabilitado = 1;
+                            //bd.Usuario.Add(oUsuario);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                rpta = 0;
+            }
+
+            return rpta;
+        }
+
+
+        //FUNCION PARA CREAR LISTAS DE PERSONAS
+        public void listaPersonas()
+        {
+            List<SelectListItem> listaPersonas = new List<SelectListItem>();
+
+            using (var bd = new BDPasajeEntities())
+            {
+                //Obtengo la lista de clientes habilitados que NO tienen usuario
+                List<SelectListItem> listaClientes = (from item in bd.Cliente
+                                                      where item.BHABILITADO == 1 && item.bTieneUsuario != 1
+                                                      select new SelectListItem
+                                                      {
+                                                          Text = item.NOMBRE + " " + item.APPATERNO + " " + item.APMATERNO + " (C)",
+                                                          Value = item.IIDCLIENTE.ToString()
+                                                      }).ToList();
+
+                //Obtengo la lista de empleados habilitados que NO tienen usuario
+                List<SelectListItem> listaEmpleados = (from item in bd.Empleado
+                                                       where item.BHABILITADO == 1 && item.bTieneUsuario != 1
+                                                       select new SelectListItem
+                                                       {
+                                                           Text = item.NOMBRE + " " + item.APPATERNO + " " + item.APMATERNO + " (E)",
+                                                           Value = item.IIDEMPLEADO.ToString()
+                                                       }).ToList();
+                //Combinamos ambas listas con AddRange
+                listaPersonas.AddRange(listaClientes);
+                listaPersonas.AddRange(listaEmpleados);
+                listaPersonas = listaPersonas.OrderBy(p => p.Text).ToList(); //Ordenamos por texto
+                listaPersonas.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+                ViewBag.listaPersona = listaPersonas;
+            }
+        }
+
+        //FUNCION PARA CREAR LISTAS DE ROLES
+        public void listarRol()
+        {
+            List<SelectListItem> listaRol;
+
+            using (var bd = new BDPasajeEntities())
+            {
+                listaRol = (from item in bd.Rol
+                            where item.BHABILITADO == 1
+                            select new SelectListItem
+                            {
+                                Text = item.NOMBRE,
+                                Value = item.IIDROL.ToString()
+                            }).ToList();
+            }
+
+            listaRol.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+
+            ViewBag.listaRol = listaRol;
         }
     }
 }
